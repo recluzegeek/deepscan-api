1f0403dcc7f3e42a6ea96a19d895a1ce093ba7e0
recluzegeek <recluzegeek@gmail.com>
2024-07-24 19:57:50 +0500

    Videos being uploaded & path stored in DB

--- routers/video.py
+++ routers/video.py
@@ -2,34 +2,21 @@
 # https://www.linode.com/docs/guides/task-queue-celery-rabbitmq/
 # https://nrsyed.com/2018/07/05/multithreading-with-opencv-python-to-improve-video-processing-performance/
 
-from fastapi import APIRouter, UploadFile, HTTPException
-
+from fastapi import APIRouter, UploadFile, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..utils.models import Video
+from ..utils.database import get_db
+from datetime import datetime
 import os
 import shutil
-
+import uuid
 
 router = APIRouter()
 
-"""
-    Video uploading endpoint. Takes an UploadFile() argument and stores in the local server storage.
-    Saves the path where the video has been stored along with the unique video uuid().
-
-    # TODO - Store video metadata in database
-    # TODO - Make protected routes
-
-"""
 @router.post("/upload")
-async def upload_video(uploaded_video: UploadFile = UploadFile(...)):
-
+async def upload_video(user_id: str, uploaded_video: UploadFile, db: Session = Depends(get_db)):
     try:
-        upload_directory = '/mnt/win/deepscan-api/storage/uploaded_videos'
-        os.makedirs(upload_directory, exist_ok=True)
-        file_path = os.path.join(upload_directory, uploaded_video.filename)
-        
-        # Save the uploaded video file
-        with open(file_path, 'wb') as video_dst:
-            shutil.copyfileobj(uploaded_video.file, video_dst)
-        
+        file_path = await save_video(user_id, uploaded_video, db)
         response_message = f"{uploaded_video.filename} video has been saved at {file_path}"
         return {
             "details": {
@@ -37,10 +24,40 @@ async def upload_video(uploaded_video: UploadFile = UploadFile(...)):
                 "path": file_path
             }
         }
-    
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to upload video: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
 
+async def save_video(user_id: str, video_file: UploadFile, db: Session):
+    upload_directory = '/mnt/win/deepscan-api/storage/uploaded_videos'
+    os.makedirs(upload_directory, exist_ok=True)
+    file_path = os.path.join(upload_directory, video_file.filename)
 
-def save_video(video_name, stored_path):
-    pass
+    # Save the uploaded video file
+    with open(file_path, 'wb') as video_dst:
+        shutil.copyfileobj(video_file.file, video_dst)
+
+    # Save video metadata in the database
+    save_video_in_db(user_id=user_id, file_path=file_path, db=db)
+
+    return file_path
+
+def save_video_in_db(user_id: str, file_path: str, db: Session):
+    try:
+        # Convert user_id to UUID
+        user_id_uuid = uuid.UUID(user_id)  # Convert the user_id string to a UUID
+
+        # Generate a new UUID for the video
+        new_video = Video(
+            id=uuid.uuid4(),  # Generate a new UUID
+            user_id=user_id_uuid,  # Use the UUID here
+            filename=os.path.basename(file_path),  # Use actual file name
+            video_storage_path=file_path,
+            status='new'
+        )
+        
+        db.add(new_video)
+        db.commit()
+        db.refresh(new_video)
+        return new_video
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail="Invalid user ID format.")
--- utils/models.py
+++ utils/models.py
@@ -1,25 +1,20 @@
-# SQLAlchemy models handle database interactions
-# SQLAlchemy models to manage database operations
-
-
 from sqlalchemy import Float, Column, ForeignKey, Integer, String, DateTime, UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-
 from .database import Base
-
 
 class Video(Base):
     __tablename__ = "uploaded_videos"
 
     id = Column(UUID, primary_key=True)
-    file_name = Column(String(4096))
-    video_url = Column(String(4096))
+    filename = Column(String(255))
+    video_storage_path = Column(String(255))
+    status = Column(String(255))
+    user_id = Column(UUID)
     upload_date_time = Column(DateTime(timezone=True), default=func.now())
     
     video_result = relationship('Classification', back_populates="video_source")
     video_report = relationship('AnalysisReport', back_populates="video_source")
-
 
 class Classification(Base):
     __tablename__ = "video_results"
@@ -30,21 +25,15 @@ class Classification(Base):
     video_id = Column(UUID, ForeignKey('uploaded_videos.id'))
     
     video_source = relationship('Video', back_populates="video_result")
-    video_report = relationship('AnalysisReport', back_populates="video_source")
-
+    video_report = relationship('AnalysisReport', back_populates="classification_source")
 
 class AnalysisReport(Base):
     __tablename__ = "video_result_reports"
 
     id = Column(UUID, primary_key=True)
-    report_url = Column(String(4096))
-    classification_id = Column(UUID, ForeignKey('video_results.id'))
+    report_url = Column(String(255))
+    classification_id = Column(UUID, ForeignKey('video_results.id'))  # Existing foreign key
+    video_id = Column(UUID, ForeignKey('uploaded_videos.id'))  # New foreign key
 
-    video_source = relationship('Classification', back_populates="video_report")
-
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    video_source = relationship('Video', back_populates="video_report")
+    classification_source = relationship('Classification', back_populates="video_report")
